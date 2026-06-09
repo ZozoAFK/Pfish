1d670888636648cd6c87024f32cecb7af2951498
Satelite-01 <raphael.tourneroche@gmail.com>
2026-06-09 15:38:13 +0200

    Correction panneau defaite

--- Proto/Script/panneau_defaite.gd
+++ Proto/Script/panneau_defaite.gd
@@ -1,25 +1,28 @@
 extends Control
 
-@onready var LVL1 = preload("res://Proto/Scene/Niv1.tscn")
-@onready var MainMenu = preload("res://Proto/Scene/menu_principal.tscn")
 # On récupère le nœud qui va jouer le son
 @onready var sfx_player: AudioStreamPlayer = $Bouttons
 
-func _on_retry_btn_button_down() -> void:
+func _ready() -> void:
+	# On force la souris à redevenir visible et utilisable pour l'UI
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func _on_retry_btn_pressed() -> void:
 	# On lance le bruitage de clic
 	sfx_player.play()
 	
-	# Ton timer de 1 seconde est parfait ici ! 
-	# Il laisse le temps au son de se jouer avant que la scène ne change.
-	await get_tree().create_timer(1).timeout
-	get_tree().change_scene_to_packed(LVL1)
+	# On attend 1 seconde pour entendre le son
+	await get_tree().create_timer(1.0).timeout
+	
+	# SOLUTION DIRECTE : On charge le fichier directement par son chemin texte
+	get_tree().change_scene_to_file("res://Proto/Scene/Niv1.tscn")
 
-
-func _on_main_menu_btn_button_down() -> void:
+func _on_main_menu_btn_pressed() -> void:
 	# On lance le bruitage de clic
 	sfx_player.play()
 	
-	# ATTENTION : Si on quitte instantanément, le son n'aura pas le temps d'être entendu.
-	# On attend un tout petit instant (0.2 seconde) pour entendre le "clic" avant de fermer.
-	await get_tree().create_timer(1).timeout
-	get_tree().change_scene_to_packed(MainMenu)
+	# On attend 1 seconde pour entendre le son
+	await get_tree().create_timer(1.0).timeout
+	
+	# SOLUTION DIRECTE : On charge le fichier directement par son chemin texte
+	get_tree().change_scene_to_file("res://Proto/Scene/menu_principal.tscn")
