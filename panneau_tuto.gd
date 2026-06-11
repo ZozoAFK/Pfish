f0978947d93a08cb1c5a084604a2737e3ff404fb
Satelite-01 <raphael.tourneroche@gmail.com>
2026-06-11 15:56:00 +0200

    Page Tuto

---
+++ panneau_tuto.gd
@@ -0,0 +1,9 @@
+extends Control
+
+@onready var LVL1 = preload("res://Proto/Scene/Niv1.tscn")
+@onready var sfx_player: AudioStreamPlayer = $Tuto_sfx
+
+func _ready() -> void:
+	sfx_player.play()
+	await get_tree().create_timer(4).timeout
+	get_tree().change_scene_to_packed(LVL1)
