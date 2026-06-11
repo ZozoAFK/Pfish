262065c179df6f5ee461701e1561a6105743f2ea
Satelite-01 <raphael.tourneroche@gmail.com>
2026-06-11 16:10:34 +0200

    TEAM_Crouton

--- panneau_tuto.gd
+++ panneau_tuto.gd
@@ -5,5 +5,5 @@ extends Control
 
 func _ready() -> void:
 	sfx_player.play()
-	await get_tree().create_timer(4).timeout
+	await get_tree().create_timer(5).timeout
 	get_tree().change_scene_to_packed(LVL1)
